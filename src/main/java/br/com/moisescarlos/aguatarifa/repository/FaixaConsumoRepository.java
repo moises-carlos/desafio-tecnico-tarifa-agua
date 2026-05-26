da1ac93f5625a1cb0eafe92b609fb8b606955424
moises-carlos <moisescarlos.239@gmail.com>
2026-05-25 23:28:13 -0300

    feat: add repositories TabelaTarifariaRepository e FaixaConsumoRepository

---
+++ src/main/java/br/com/moisescarlos/aguatarifa/repository/FaixaConsumoRepository.java
@@ -0,0 +1,17 @@
+package br.com.moisescarlos.aguatarifa.repository;
+
+import br.com.moisescarlos.aguatarifa.model.FaixaConsumo;
+import br.com.moisescarlos.aguatarifa.model.enums.TipoCategoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface FaixaConsumoRepository extends JpaRepository<FaixaConsumo, UUID> {
+
+    List<FaixaConsumo> findByCategoriaConsumoTipoAndCategoriaConsumoTabelaTarifaId(
+            TipoCategoria tipo,
+            UUID tabelaTarifariaId
+    );
+}
