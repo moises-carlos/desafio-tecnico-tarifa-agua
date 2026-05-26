2fdc859f25af91384aca3a9cd79b9b1c9afe92c9
moises-carlos <moisescarlos.239@gmail.com>
2026-05-25 21:16:36 -0300

    feat: inicializa estrutura do projeto spring

---
+++ src/main/java/br/com/moisescarlos/aguatarifa/AguaTarifaApplication.java
@@ -0,0 +1,13 @@
+package br.com.moisescarlos.aguatarifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AguaTarifaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AguaTarifaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/moisescarlos/aguatarifa/AguaTarifaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moisescarlos.aguatarifa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AguaTarifaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
