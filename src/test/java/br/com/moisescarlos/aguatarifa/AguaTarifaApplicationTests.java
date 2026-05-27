6130386a95015e53f5759659ed0a4775ff1be00d
moises-carlos <moisescarlos.239@gmail.com>
2026-05-27 01:14:34 -0300

    fix: remover SpringBootTest para evitar falha no build sem banco configurado

--- src/test/java/br/com/moisescarlos/aguatarifa/AguaTarifaApplicationTests.java
+++ src/test/java/br/com/moisescarlos/aguatarifa/AguaTarifaApplicationTests.java
@@ -1,9 +1,7 @@
 package br.com.moisescarlos.aguatarifa;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AguaTarifaApplicationTests {
 
     @Test
