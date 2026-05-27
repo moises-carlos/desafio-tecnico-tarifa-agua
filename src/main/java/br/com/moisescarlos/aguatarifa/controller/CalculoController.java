117200e024dbd5e9561df007d62a009309f08827
moises-carlos <moisescarlos.239@gmail.com>
2026-05-26 22:43:38 -0300

    feat: add controllers TabelaTarifariaController e CalculoController

---
+++ src/main/java/br/com/moisescarlos/aguatarifa/controller/CalculoController.java
@@ -0,0 +1,28 @@
+package br.com.moisescarlos.aguatarifa.controller;
+
+import br.com.moisescarlos.aguatarifa.dto.request.CalculoRequest;
+import br.com.moisescarlos.aguatarifa.dto.response.CalculoResponse;
+import br.com.moisescarlos.aguatarifa.service.CalculoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/calculos")
+@RequiredArgsConstructor
+public class CalculoController {
+
+    private final CalculoService service;
+
+    @PostMapping("/{tabelaId}")
+    public ResponseEntity<CalculoResponse> calcular(
+            @PathVariable UUID tabelaId,
+            @RequestBody @Valid CalculoRequest request
+    ) {
+        CalculoResponse response = service.calcular(request, tabelaId);
+        return ResponseEntity.ok(response);
+    }
+}
